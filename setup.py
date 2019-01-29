3564ea5cd61392a23d98118e3e984cc35bcd3ae3
ileler <ileler@qq.com>
2019-01-29 14:47:38 +0800

    support py3

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="iu-jar",
-    version="0.0.2",
+    version="0.0.3",
     author="ileler",
     author_email="kerwin612@qq.com",
     description="An incremental upgrade tool for jar packages",
@@ -14,7 +14,7 @@ setuptools.setup(
     url="https://github.com/ileler/iu-jar",
     packages=setuptools.find_packages(),
     classifiers=(
-        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 2",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ),
